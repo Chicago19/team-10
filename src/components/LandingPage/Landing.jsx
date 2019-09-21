6cc4d0f356021f033bd8503c91dc643de29a64d7
Jimmy Dang <immmjimmy@gmail.com>
2019-09-21 00:34:41 -0700

    Revert "Jimmy"

--- src/components/LandingPage/Landing.jsx
+++ src/components/LandingPage/Landing.jsx
@@ -2,13 +2,10 @@ import React from "react";
 
 import Hero from "./Hero";
 import About from "./About";
-import Nav from "./Nav";
 
 const Landing = () => {
   return (
-
     <div className="fullHeight">
-      <Nav />
       <Hero />
       <About />
     </div>
