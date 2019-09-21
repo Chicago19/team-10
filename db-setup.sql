0e9811154f70f2cbd511dc255769de71e773ce5b
Justin Schmitz <jschmitz2@hawk.iit.edu>
2019-09-21 05:36:22 -0500

    Update db-setup.sql

--- db-setup.sql
+++ db-setup.sql
@@ -10,8 +10,10 @@ CREATE TABLE profile(
 CREATE TABLE class(
 	email varchar(30) REFERENCES profile(email),
 	class_name varchar(30),
+	class_year char(4),
+	class_semester varchar(10),
 	score varchar(10),
-	PRIMARY KEY (email, class_name)
+	PRIMARY KEY (email, class_name, class_year, class_semester)
 );
 
 CREATE TABLE official(
@@ -32,3 +34,5 @@ CREATE TABLE official(
 
 	PRIMARY KEY (email)
 );
+
+# C:\Users\jjsch\Documents\GitHub\team-10
