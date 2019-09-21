beac576b8368707175404fce084044ca386a014d
Justin Schmitz <jschmitz2@hawk.iit.edu>
2019-09-21 05:39:33 -0500

    Update db-setup.sql

--- db-setup.sql
+++ db-setup.sql
@@ -9,12 +9,19 @@ CREATE TABLE profile(
 
 CREATE TABLE class(
 	email varchar(30) REFERENCES profile(email),
-	class_name varchar(30),
+	class_name varchar(30) REFERENCES courses(class_name),
 	class_year char(4),
 	class_semester varchar(10),
 	score varchar(10),
 	PRIMARY KEY (email, class_name, class_year, class_semester)
 );
+
+CREATE TABLE courses(
+	class_name VARCHAR(30),
+	class_year char(4),
+	class_semester varchar(10),
+	PRIMARY KEY (class_name, class_year, class_semester)
+)
 
 CREATE TABLE official(
 	email varchar(30) REFERENCES profile(email),
