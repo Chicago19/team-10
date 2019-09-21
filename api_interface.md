3f2273d340eee7ef7bcfabb361ac8ba09c6902be
Justin Schmitz <jschmitz2@hawk.iit.edu>
2019-09-21 02:24:10 -0500

    Update Serve.py, api_interface.md
    Updated with more routes, also renamed the database file to __init__.py so it can be imported as a module. Then used a python linter to make conform to styleguides for readability

--- api_interface.md
+++ api_interface.md
@@ -6,12 +6,14 @@ All requests support POST and GET.
 
 ### `\` - The Test One
 
-**Inputs**: None taken, but if you send some, it'll stil work.
+**Inputs**: None taken, but if you send some, it'll still work.
+
 **Outputs**: `{"test": True}`
 
 ### `/calendar` - Read Calendar Information
 
 **Inputs**: `cal_filter: search_query`, where `search_query` is a string.
+
 **Outputs**: JSON of form:
 
 ```JSON
@@ -40,4 +42,39 @@ Note that other attributes are also present - reference https://developers.googl
 ### `/score` - Scores a user's exam
 
 **Inputs**: `"answers": [1,1,2,4]`
+
 **Outputs**: `{'score': score}`
+
+### `/create_profile` - Creates a user's profile
+
+**Inputs**:
+
+```JSON
+{
+  "name": "full name",
+  "email": "email address",
+  "referral": "referral name",
+  "phone": "phone number",
+  "password": "plaintext_password",
+  "age": "age"
+}
+```
+**Outputs**: `{"score": int}`
+
+
+### `/auth_path` - Authenticate a user
+
+**Inputs**:
+```JSON
+{
+  "email": "email",
+  "password": "password"
+}
+```
+**Outputs**: `{"result": boolean}`
+
+### `/email_test` - Check if an account already exists
+
+**Inputs**: `{"email": "email_address"}`
+
+**Outputs**: `{"result": boolean}`
--- serve.py
+++ serve.py
@@ -1,25 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 from calendar_integration import Session
-import json
+from backend import backend_profile
 
-app = Flask(__name__, static_folder="build/static", template_folder="build")
+backend = backend_profile('cfg10', 'codeforgood', 'localhost', 5432, 'cfg_10')
+
+app = Flask(__name__, static_folder='build/static', template_folder='build')
 cal = Session()
 
-@app.route("/", methods=['POST', 'GET'])
-def hello():
-    return {"test": True}
 
-@app.route("/calendar", methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
+def hello():
+    return {'test': True}
+
+
+@app.route('/calendar', methods=['POST', 'GET'])
 def calendar():
     event_name = request.json['cal_filter']
     print(event_name)
     print(type(event_name))
     return {'result': cal.search_events(event_name)}
 
-@app.route("/score", methods=['POST', 'GET'])
+
+@app.route('/score', methods=['POST', 'GET'])
 def scoring():
     user_score = request.json['answers']
-    correct_score = [1,1,2,3,4]
+    correct_score = [1, 1, 2, 3, 4]
     score = 0
     for vals in zip(user_score, correct_score):
         if vals[0] == vals[1]:
@@ -27,9 +32,33 @@ def scoring():
     return {'score': score}
 
 
+@app.route('/create_profile', methods=['POST', 'GET'])
+def add_profile():
+    profile_data = request.json
+    if backend.insert_profile(profile_data):
+        return {'return': True}
+    else:
+        return {'return': False}
 
 
+@app.route('/auth_path', methods=['POST', 'GET'])
+def authenticate():
+    profile = request.json
+    if backend.__check_login_password(profile['email'], profile['password']):
+        return {"result": True}
+    else:
+        return {"result": False}
 
-if __name__ == "__main__":
+
+@app.route('/email_test', methods=['POST', 'GET'])
+def email_test():
+    data = request.json
+    if backend.check_email(data['email']):
+        return {"result": True}
+    else:
+        return {"result": False}
+
+
+if __name__ == '__main__':
     app.debug = True
     app.run(host='199.253.248.17', port=5005)
