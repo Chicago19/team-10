148dcc4bad0fe8ed6805a741dad48114b034696e
Justin Schmitz <jschmitz2@hawk.iit.edu>
2019-09-20 21:21:17 -0500

    Create and add db-setup.sql
    Create and add db-setup.sql

---
+++ db-setup.sql
@@ -0,0 +1,32 @@
+CREATE TABLE profile(
+	name varchar(30),
+	email varchar(30) PRIMARY KEY,
+	referral varchar(30),
+	phone varchar(13),
+	password char(42)
+);
+
+CREATE TABLE class(
+	email varchar(30) REFERENCES profile(email),
+	class_name varchar(30),
+	score varchar(10),
+	PRIMARY KEY (email, class_name)
+);
+
+CREATE TABLE official(
+	email varchar(30) REFERENCES profile(email),
+	registrtation_date varchar(30),
+	keyword varchar(30),
+	oral_score varchar(30),
+	written_level varchar(30),
+	test_date varchar(30),
+	best_exam varchar(30),
+	iccb_intake_form varchar(30),
+	empowerment_survey varchar(30),
+	transportation varchar(10),
+	site_location varchar(15),
+	sig_date timestamp,
+	signed boolean,
+
+	PRIMARY KEY (email)
+);
