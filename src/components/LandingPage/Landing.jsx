a9be9ba895dd627539ab1776f9524bff153e5657
immmjimmy <immmjimmy@gmail.com>
2019-09-20 20:10:57 -0700

    Rough draft of landing page

---
+++ src/components/LandingPage/Landing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Hero from "./Hero";
+import About from "./About";
+
+const Landing = () => {
+  return (
+    <div className="fullHeight">
+      <Hero />
+      <About />
+    </div>
+  );
+};
+
+export default Landing;
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -3,13 +3,14 @@ import PropTypes from "prop-types";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "../App";
+import Landing from "./LandingPage/Landing";
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router>
       <Switch>
         <Route path="/hello" component={App} />
-
+        <Route path="/landing" component={Landing} />
       </Switch>
     </Router>
   </Provider>
